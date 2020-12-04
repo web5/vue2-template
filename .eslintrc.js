f871909955a891e585d7306a4298a60475eb9a08
geekwen <geekwen@tencent.com>
2020-12-04 22:03:45 +0800

    feat: 整合prettier,vue,eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,32 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ["plugin:vue/essential", "prettier", "prettier/vue", "plugin:vue/base"],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/html-indent": ["error", 2, {
+      "attribute": 1,
+      "baseIndent": 0,
+      "closeBracket": 0,
+      "alignAttributesVertically": true,
+      "ignores": []
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "always"
+    }],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 3,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }],
+    singleQuote: true, // 使用单引号（默认false）
   },
 };
